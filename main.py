a4ab11ff5c9a79482e2c2e52b90436d211d9b397
Jonas-dbruyne <fernandezjonas41@gmail.com>
2024-10-16 16:25:02 -0400

    Tarea de la clase anterior

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from suma import suma
+from resta import resta
 
 def main():
     while True:
         print("Operaciones matematicas:")
         print("1. Suma de dos números")
+        print("2. Resta de dos números")
         print("0. salir")
         
         opcion = input("elige una opcion: ")
@@ -16,6 +18,9 @@ def main():
         
         if opcion == '1':
             print("resultado: ", suma(a, b))
+        if opcion == '2':
+            print("resultado: ", resta(a, b))    
+        
             
 if __name__== "__main__":
     main()
